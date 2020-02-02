44d8fe17c86e76233b11bf4ef05e6084ce279316
mar4uk09 <yuliyamar4uk@mail.ru>
2020-02-02 11:43:35 +0300

    add correction of mistakes

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var MIN_COMMENTS_NUMBER = 1;
 var MAX_COMMENTS_NUMBER = 5;
 var MIN_NUMBER_OF_SENTENCES = 1;
 var MAX_NUMBER_OF_SENTENCES = 2;
-var MESSAGE_VARIETIES = [
+var MESSAGE_VARIANTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -17,7 +17,7 @@ var MESSAGE_VARIETIES = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-var NAMES_VARIETIES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var NAMES_VARIANTS = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
 var picturesContainer = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
@@ -28,15 +28,13 @@ var getRandomInt = function (min, max) {
 
 var generateMessage = function (min, max, messageArray) {
   var messageArr = [];
-  var randomInt = getRandomInt(min, max);
+  var numberOfSentences = getRandomInt(min, max);
 
-  for (var i = 0; i < randomInt; i++) {
+  for (var i = 0; i < numberOfSentences; i++) {
     messageArr.push(messageArray[getRandomInt(0, messageArray.length - 1)]);
   }
 
-  var messageString = messageArr.reduce(function (sum, current) {
-    return sum + ' ' + current;
-  });
+  var messageString = messageArr.join(' ');
 
   return messageString;
 };
@@ -45,7 +43,7 @@ var generateComment = function (messageArray, nameArray) {
   var comment = {};
 
   comment.avatar = 'img/avatar-' + getRandomInt(MIN_AVATAR_NUMBER, MAX_AVATAR_NUMBER) + '.svg';
-  comment.message = generateMessage(MIN_NUMBER_OF_SENTENCES, MAX_NUMBER_OF_SENTENCES, MESSAGE_VARIETIES);
+  comment.message = generateMessage(MIN_NUMBER_OF_SENTENCES, MAX_NUMBER_OF_SENTENCES, MESSAGE_VARIANTS);
   comment.name = nameArray[getRandomInt(0, nameArray.length - 1)];
   return comment;
 };
@@ -59,7 +57,7 @@ var generatePictureDescription = function (num) {
   pictureDescription.comments = [];
 
   for (var i = 0; i < getRandomInt(MIN_COMMENTS_NUMBER, MAX_COMMENTS_NUMBER); i++) {
-    pictureDescription.comments.push(generateComment(MESSAGE_VARIETIES, NAMES_VARIETIES));
+    pictureDescription.comments.push(generateComment(MESSAGE_VARIANTS, NAMES_VARIANTS));
   }
 
   return pictureDescription;
