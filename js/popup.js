c8fea517fc572d565aaa75f972d515708d060bfd
mar4uk09 <yuliyamar4uk@mail.ru>
2020-03-20 00:33:10 +0300

    add effect script

--- js/popup.js
+++ js/popup.js
@@ -6,10 +6,12 @@ var uploadFileElement = document.querySelector('#upload-file');
 var uploadOverlayElement = document.querySelector('.img-upload__overlay');
 var uploadCancelButton = uploadOverlayElement.querySelector('#upload-cancel');
 var hashtagsInput = uploadOverlayElement.querySelector('.text__hashtags');
+var textarea = uploadOverlayElement.querySelector('.text__description');
 var pageBody = document.querySelector('body');
+var imgPreview = document.querySelector('.img-upload__preview img');
 
 var onPopupEscPress = function (evt) {
-  if (evt.key === ESC_KEY && hashtagsInput !== document.activeElement) {
+  if (evt.key === ESC_KEY && hashtagsInput !== document.activeElement && textarea !== document.activeElement) {
     closePopup();
   }
 };
@@ -24,6 +26,7 @@ var closePopup = function () {
   pageBody.classList.remove('modal-open');
   uploadOverlayElement.classList.add('hidden');
   uploadFileElement.value = '';
+  imgPreview.style.transform = 'scale(1.0)';
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
